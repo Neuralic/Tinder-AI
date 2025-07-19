c8de4bdb9a74c153dc091b79a3ac8a4739b843c1
Buoyancy <oluwaseyidavid2005@gmail.com>
2025-07-19 13:48:11 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gray-900': '#121826',
+        'gray-800': '#1e293b',
+        'rose-500': '#f43f5e',
+        'amber-500': '#f59e0b',
+      }
+    },
   },
   plugins: [],
-}
-
-// File: frontend/postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
 }
