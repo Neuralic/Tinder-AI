90016c5482b726252569aa10e881508d247780ad
Buoyancy <oluwaseyidavid2005@gmail.com>
2025-07-19 17:57:59 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [],
+    unoptimized: true
   },
+  trailingSlash: true,
+  output: 'export',
+  distDir: 'out'
 }
+
+module.exports = nextConfig
